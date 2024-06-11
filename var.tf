4dcb23eeb096fa67f0dedcd28508dca673c721b9
ravigopal94-g <60359771+ravigopal94-g@users.noreply.github.com>
2024-06-11 17:13:16 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,19 @@
+variable "endpoint" {
+  type    = string
+  default = "https://apac.smartkey.io"
+}
+
+variable "username" {
+  type    = string
+  default = "ravi.gopal@fortanix.com"
+}
+
+variable "pass" {
+  type    = string
+  default = "${var.password}"
+}
+
+variable "acct_id" {
+  type    = string
+  default = "8e3ef205-f3d1-4a2c-876b-cd3ea5ce04e2"
+}
